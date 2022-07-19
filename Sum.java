620abc1060b7d6f61e328bb9950a9700e3b9d4e1
Abhijeet_Bagade <abhibagade96@gmail.com>
2022-07-19 18:04:02 +0530

    First Commit of Code

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class sum{
+ 	public static void miain{
+
+    int x=57;
+    int y=98;
+    int z=66; 
+    int Sum;
+  
+    Sum=x+y+z;
+    
+    System.out.println("The Sum of three number is "+Sum);
+}
+}
